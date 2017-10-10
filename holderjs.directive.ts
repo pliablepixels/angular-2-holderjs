bf3333b7852e6781ba1382e94da71a0e0be18e97
Pliable Pixels <pliablepixels@users.noreply.github.com>
2017-10-10 14:17:27 -0400

    Update holderjs.directive.ts

--- holderjs.directive.ts
+++ holderjs.directive.ts
@@ -1,11 +1,16 @@
 // credit: https://stackoverflow.com/a/46673111/1361529
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+declare var Holder: any;
+
 @Directive({
   selector: '[holderjs]',
 })
 export class HolderjsDirective {
-  constructor(public el: ElementRef) {}
+    constructor(public el: ElementRef, public ren:Renderer2) {
+       //Holder.run({images:el.nativeElement});
+    }
 
-  ngAfterViewInit() {
-     Holder.run({images:this.el.nativeElement});
-  }
+    ngAfterViewInit() {
+        Holder.run({images:this.el.nativeElement});
+    }
 }
