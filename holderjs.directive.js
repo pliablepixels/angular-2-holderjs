08ff5a5c60984611ea7b58137896eb76347c3ac5
Pliable Pixels <pliablepixels@gmail.com>
2017-10-10 14:39:15 -0400

    updates

--- holderjs.directive.js
+++ holderjs.directive.js
@@ -5,21 +5,23 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-var core_1 = require('@angular/core');
-var HolderjsDirective = (function () {
-    function HolderjsDirective(el) {
-        Holder.run({ images: el.nativeElement });
+exports.__esModule = true;
+// credit: https://stackoverflow.com/a/46673111/1361529
+var core_1 = require("@angular/core");
+var HolderjsDirective = /** @class */ (function () {
+    function HolderjsDirective(el, ren) {
+        this.el = el;
+        this.ren = ren;
+        //Holder.run({images:el.nativeElement});
     }
+    HolderjsDirective.prototype.ngAfterViewInit = function () {
+        Holder.run({ images: this.el.nativeElement });
+    };
     HolderjsDirective = __decorate([
         core_1.Directive({
             selector: '[holderjs]'
-        }), 
-        __metadata('design:paramtypes', [core_1.ElementRef])
+        })
     ], HolderjsDirective);
     return HolderjsDirective;
 }());
 exports.HolderjsDirective = HolderjsDirective;
-//# sourceMappingURL=holderjs.directive.js.map
